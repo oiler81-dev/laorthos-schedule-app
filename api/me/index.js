b76e6c2bc587094e195601caba1f792dda5f17fc
oiler81-dev <oiler81@gmail.com>
2026-03-02 13:51:49 -0800

    Update index.js

--- api/me/index.js
+++ api/me/index.js
@@ -1,1 +1,24 @@
+module.exports = async function (context, req) {
+  const headers = req.headers || {};
 
+  const email =
+    headers["x-ms-client-principal-name"] ||
+    headers["X-MS-CLIENT-PRINCIPAL-NAME"] ||
+    null;
+
+  const editors = new Set([
+    "nperez@unitymsk.com",
+    "aledezma@laorthos.com"
+  ]);
+
+  const isEditor = email ? editors.has(email.toLowerCase()) : false;
+
+  context.res = {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+    body: {
+      email,
+      editor: isEditor
+    }
+  };
+};
